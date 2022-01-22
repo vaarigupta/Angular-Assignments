64f468a771962eb01ccf19763b7934568f889eba
Vaari Gupta <vaarigupta24@gmail.com>
2022-01-23 04:21:54 +0530

    Added product and used directives

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  
+  IsClicked:boolean = false;
+  count:number = 0;
+  nums: number[] = [];
+  bgcolor:string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  ToggleDetails()
+  {
+    this.IsClicked = !this.IsClicked;
+    this.count++;
+    this.bgcolor = this.count>=5?'blue':'transparent';
+    this.nums.push(this.count);
+  }
+
+}
